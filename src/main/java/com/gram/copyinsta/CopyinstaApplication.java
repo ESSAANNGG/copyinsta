0f68fe526e976bdf6fcc5d6af83d62ac34a804a9
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-17 20:45:56 +0900

    Initial commit

---
+++ src/main/java/com/gram/copyinsta/CopyinstaApplication.java
@@ -0,0 +1,13 @@
+package com.gram.copyinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyinstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyinstaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gram/copyinsta/CopyinstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gram.copyinsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyinstaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
