58d84da6d339a8be0ee796a4fc2e61d4717eceba
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-17 21:08:07 +0900

    회원 테이블 구성중

---
+++ src/main/java/com/gram/copyinsta/entity/User.java
@@ -0,0 +1,15 @@
+package com.gram.copyinsta.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@ToString
+
+public class User {
+}
