15f1c3c2d05f92260635a0059bf4e24e01aa56b8
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-24 17:37:59 +0900

    컨트롤러 등록

--- src/main/java/com/gram/copyinsta/controller/MemberController.java
+++ src/main/java/com/gram/copyinsta/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.gram.copyinsta.controller;
 
+import com.gram.copyinsta.dto.UserDto;
 import com.gram.copyinsta.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
@@ -18,5 +20,40 @@ public class MemberController {
     @GetMapping("/user/signup")
     public String dispSignup(){
         return "/signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String execSignup(UserDto userDto){
+        return null;
+    }
+
+    @GetMapping("/user/login")
+    public String login(){
+        return "/login";
+    }
+
+    @GetMapping("/user/login/result")
+    public String loginResult() {
+        return "/loginSuccess";
+    }
+
+    @GetMapping("/user/logout/result")
+    public String logout() {
+        return "/logout";
+    }
+
+    @GetMapping("/user/denied")
+    public String denied() {
+        return "/denied";
+    }
+
+    @GetMapping("/user/info")
+    public String UserInfo() {
+        return "/userinfo";
+    }
+
+    @GetMapping("/admin")
+    public String Admin() {
+        return "/admin";
     }
 }
