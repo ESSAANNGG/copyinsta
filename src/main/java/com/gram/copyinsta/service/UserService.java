e949abf3b850324ce01e7c07bdd464d6781db03f
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-05-24 17:51:56 +0900

    비밀번호 암호화 서비스코드 추가

--- src/main/java/com/gram/copyinsta/service/UserService.java
+++ src/main/java/com/gram/copyinsta/service/UserService.java
@@ -1,10 +1,26 @@
 package com.gram.copyinsta.service;
 
+import com.gram.copyinsta.dto.UserDto;
+import com.gram.copyinsta.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.transaction.Transactional;
 
 public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Transactional
+    public Long joinUser(UserDto userDto){
+        //비밀번호 암호화
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        userDto.setPw(passwordEncoder.encode(userDto.getPw()));
+
+        return userRepository.save(userDto.toEntity()).getId();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
