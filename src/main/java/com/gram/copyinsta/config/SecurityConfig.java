336bd50c6dcad527dbbfa90181a6093c18b2255e
ESSAANNGG <tkdwnsdk11@gmail.com>
2022-06-29 10:13:15 +0900

    어노테이션추가

--- src/main/java/com/gram/copyinsta/config/SecurityConfig.java
+++ src/main/java/com/gram/copyinsta/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
